d744b839e0a33747d09f0b57c52ad4c6d3cad87e
palewire <ben.welsh@gmail.com>
2014-07-27 21:55:36 -0700

    test fixes

--- storysniffer/__init__.py
+++ storysniffer/__init__.py
@@ -8,15 +8,17 @@ except ImportError:
 # Drawn from Django source code:
 # https://github.com/django/django/blob/master/django/core/validators.py
 URL_REGEX = re.compile(
-    r'^(?:[a-z0-9\.\-]*)://' # scheme is validated separately
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}(?<!-)\.?)|' # domain...
-    r'localhost|' # localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|' # ...or ipv4
-    r'\[?[A-F0-9]*:[A-F0-9:]+\]?)' # ...or ipv6
-    r'(?::\d+)?' # optional port
+    r'^(?:[a-z0-9\.\-]*)://'  # scheme is validated separately
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|\
+[A-Z0-9-]{2,}(?<!-)\.?)|'  # domain...
+    r'localhost|'  # localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
+    r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
+    r'(?::\d+)?'  # optional port
     r'(?:/?|[/?]\S+)$',
     re.IGNORECASE
 )
+
 
 def guess(url):
     """
@@ -25,3 +27,5 @@ def guess(url):
     """
     if not URL_REGEX.search(url):
         raise ValueError("Provided url does not match acceptable URL patterns")
+
+    urlparse(url)
