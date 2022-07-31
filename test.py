c6737323f8c591c07718428bf190d05bdd278944
palewire <b@palewi.re>
2022-07-31 16:45:53 -0700

    Switch to pytest

--- test.py
+++ test.py
@@ -1,51 +1,36 @@
 """Test the module."""
-import unittest
-
 from storysniffer import StorySniffer
 
 
-class SniffTest(unittest.TestCase):
-    """Run basic tests."""
+def test_guess():
+    """Test guesses."""
+    client = StorySniffer()
+    func = client.guess
 
-    def setUp(self):
-        """Configure common settings."""
-        self.yes = "http://www.washingtonpost.com/investigations/us-intelligence-mining-data-from-nine-us-internet-companies-in-broad-secret-program/2013/06/06/3a0c0da8-cebf-11e2-8845-d970ccb04497_story.html"
-        self.absolute_yes = "/investigations/us-intelligence-mining-data-from-nine-us-internet-companies-in-broad-secret-program/2013/06/06/3a0c0da8-cebf-11e2-8845-d970ccb04497_story.html"
-        self.no = "http://www.cnn.com/"
-        self.absolute_no = "/"
-        self.busted = "foobar"
-        self.client = StorySniffer()
+    yes = "http://www.washingtonpost.com/investigations/us-intelligence-mining-data-from-nine-us-internet-companies-in-broad-secret-program/2013/06/06/3a0c0da8-cebf-11e2-8845-d970ccb04497_story.html"
+    absolute_yes = "/investigations/us-intelligence-mining-data-from-nine-us-internet-companies-in-broad-secret-program/2013/06/06/3a0c0da8-cebf-11e2-8845-d970ccb04497_story.html"
+    no = "http://www.cnn.com/"
+    absolute_no = "/"
+    busted = "foobar"
 
-    def test_guess(self):
-        """Test guesses."""
-        func = self.client.guess
-        self.assertTrue(func(self.yes))
-        self.assertFalse(func(self.no))
-        self.assertTrue(func(self.absolute_yes))
-        self.assertFalse(func(self.busted))
-        self.assertFalse(func("http://www.facebook.com/foobar/"))
-        self.assertFalse(func("http://careers.cnn.com/foobar/"))
-        self.assertFalse(func("http://www.news.xxx/foobar/"))
-        self.assertFalse(func("http://www.news.com/foobar.css"))
-        self.assertFalse(func("http://www.news.com/foobar.jpg"))
-        self.assertFalse(func("http://www.news.com/foobar.js"))
-        self.assertTrue(
-            func(
-                "http://www.latimes.com/local/westside/la-me-lightning-strikes-20140728-story.html"
-            )
-        )
-        self.assertTrue(
-            func("http://www.latimes.com/local/la-me-water-use-war-20140727-story.html")
-        )
-        self.assertFalse(func("http://www.latimes.com/opinion/"))
-        self.assertFalse(func("http://www.latimes.com/opinion/editorials/"))
-        self.assertTrue(func("http://www.news.com/story/story.html"))
-        self.assertTrue(
-            func(
-                "http://www.cnn.com/video/data/2.0/video/us/2014/07/23/hln-husband-emails-sex-spreadsheet.hln.html?hpt=hp_t4"
-            )
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
+    assert func(yes)
+    assert func(no) is not True
+    assert func(absolute_yes)
+    assert func(absolute_no) is not True
+    assert func(busted) is not True
+    assert func("http://www.facebook.com/foobar/") is not True
+    assert func("http://careers.cnn.com/foobar/") is not True
+    assert func("http://www.news.xxx/foobar/") is not True
+    assert func("http://www.news.com/foobar.css") is not True
+    assert func("http://www.news.com/foobar.jpg") is not True
+    assert func("http://www.news.com/foobar.js") is not True
+    assert func(
+        "http://www.latimes.com/local/westside/la-me-lightning-strikes-20140728-story.html"
+    )
+    assert func("http://www.latimes.com/local/la-me-water-use-war-20140727-story.html")
+    assert func("http://www.latimes.com/opinion/") is not True
+    assert func("http://www.latimes.com/opinion/editorials/") is not True
+    assert func("http://www.news.com/story/story.html")
+    assert func(
+        "http://www.cnn.com/video/data/2.0/video/us/2014/07/23/hln-husband-emails-sex-spreadsheet.hln.html?hpt=hp_t4"
+    )
